5b2424e9e701e90b2136f97744e6bc0f8fad93a8
assomatic <90207584+assomatic@users.noreply.github.com>
2022-08-04 14:19:11 +0200

    seventh son

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ const links = [
     url: 'https://www.gatsbyjs.com/docs/tutorial/',
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
+    color: '#32cd32',
   },
   {
     text: 'How to Guides',
