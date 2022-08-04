2e0f405b4b57cc72f6e645f229d45532526b8eba
assomatic <90207584+assomatic@users.noreply.github.com>
2022-08-04 15:53:19 +0200

    End of Part <III>

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `pewag-merch-shop`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt='I am master chainsaw!'
+        src='../images/pexels-karolina-grabowska-4206123.jpg'
+      ></StaticImage>
     </Layout>
   )
 }
