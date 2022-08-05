fbc4e17a39f22795c7b58d930dab6858e4e8590e
assomatic <90207584+assomatic@users.noreply.github.com>
2022-08-05 20:51:32 +0200

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,14 +1,23 @@
 import * as React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{' '}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
-      <Link to='/blog/'>Back to Blog</Link>
     </Layout>
   )
 }
@@ -19,10 +28,17 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
   }
 `
-
 export default BlogPost
